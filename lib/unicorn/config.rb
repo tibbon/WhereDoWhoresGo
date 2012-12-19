b790c879a1a3fa8effba600b840fc36d55a183ec
David Fisher <tibbon@gmail.com>
2012-12-19 00:22:38 -0500

    Updated Unicorn config

--- lib/unicorn/config.rb
+++ lib/unicorn/config.rb
@@ -1,36 +1,16 @@
 # What the timeout for killing busy workers is, in seconds
 timeout 60
-
+ 
 # Whether the app should be pre-loaded
 preload_app true
-
-# How many worker processes
-worker_processes 3
-
-before_fork do |server, worker|
-  # Replace with MongoDB or whatever
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
-  end
-  
-  # If you are using Redis but not Resque, change this
-  if defined?(Resque)
-    Resque.redis.quit
-    Rails.logger.info('Disconnected from Redis')
-  end
  
+# How many worker processes
+worker_processes 4
+ 
+# before/after forks
+before_fork do |server, worker|
   sleep 1
 end
- 
 after_fork do |server, worker|
-  if defined?(ActiveRecord::Base)
-    ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
-  end
-  
-  if defined?(Resque)
-    Resque.redis = ENV['REDIS_URI']
-    Rails.logger.info('Connected to Redis')
-  end
+  GC.disable
 end
