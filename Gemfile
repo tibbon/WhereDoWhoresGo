fda796250f86ff668dec90577c79d4af6aa624ef
David Fisher <tibbon+github@gmail.com>
2013-03-22 11:43:04 -0400

    Attempting to update to Ruby 2.0 and Rails 4.0. Currently a fail

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
+ruby '2.0.0'
 
-gem 'rails', '3.2.12'
+gem 'rails', '3.2.13'
 gem 'unicorn'
 gem 'sqlite3', groups: [:development, :test]
 gem 'pg', groups: [:production]
--- app/controllers/whores_controller.rb
+++ app/controllers/whores_controller.rb
@@ -4,7 +4,9 @@ class WhoresController < ApplicationController
   def index
     @whore = Whore.new
     @whores = Whore.order("id DESC").page params[:page]
-    @whore_count = Whore.count
+    @whore_count = Rails.cache.fetch([:whore_count], expires_in: 30.minutes) do
+      Whore.count
+    end
   end
   
   def create
